4b1d6eb7c237d76af2a1594543028b15954c7c4c
balagrivine <balagrivine@gmail.com>
2023-06-16 02:24:22 +0300

    write out ouyput without using printf

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 1 (success)
+ */
+int main(void)
+
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
