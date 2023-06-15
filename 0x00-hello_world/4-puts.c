636fe0bdb4022254a35eeff27a3594da8e8dd95c
balagrivine <balagrivine@gmail.com>
2023-06-16 00:15:25 +0300

    put the return value of main to be zero

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
 *main - main entry point
-*return (0);
 *return : always o (success)
 */
 int main(void)
 {
+	return (0);
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
