2a28a74f27b1783dcafd2fe4e1ab4b02444c5a38
balagrivine <balagrivine@gmail.com>
2023-06-16 00:18:35 +0300

    try to fix the bug

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
-*main - main entry point
-*return : always o (success)
-*/
+ *main - main entry point
+ *return : always o (success)
+ */
 int main(void)
 {
-	return (0);
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
