fe82ffc357e021f481e9de2978617e60e4142399
balagrivine <balagrivine@gmail.com>
2023-06-15 23:40:46 +0300

    correction of an error in the script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
-#include<stdio.h>
-
-int main()
+#include <stdio.h>
+/**
+*main - main entry point
+*return : always o (success)
+*/
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}	
+}
