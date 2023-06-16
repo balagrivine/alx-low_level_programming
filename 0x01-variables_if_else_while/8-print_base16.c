1db3bfc93cc1852cf6fd60aa02bb1eb3dcb52bcf
balagrivine <balagrivine@gmail.com>
2023-06-17 00:15:59 +0300

    print hexadecimal values

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 16; i++)
+	{
+		if (i < 10)
+		{
+			putchar('0' + i);
+		}
+		else
+		{
+			putchar('a' + (i - 10));
+		}
+	}
+	putchar('\n');
+	return (0);
+}
