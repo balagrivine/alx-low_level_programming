ee8348796c9adcc0ede86c42085f9dee86682e2a
balagrivine <balagrivine@gmail.com>
2023-06-17 00:59:54 +0300

    print combination of single numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar('0' + a);
+		putchar(',');
+		putchar(' ');
+
+	}
+	putchar('\n');
+	return (0);
+
+}
