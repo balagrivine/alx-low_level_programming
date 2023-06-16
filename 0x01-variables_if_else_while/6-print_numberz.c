136aa939e15fb75a1682067b4a1dbf003c9df573
balagrivine <balagrivine@gmail.com>
2023-06-16 23:19:59 +0300

    print an array of 10 integers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (succcess)
+ */
+
+int main(void)
+{
+	int num[10] = {0,1,2,3,4,5,6,7,8,9};
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar (num[i]);
+	}
+	return (0);
+
+}
