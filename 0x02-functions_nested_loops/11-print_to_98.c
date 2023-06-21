3151a3af99a2a00716aaf16dbd6f4f0c74feba2c
balagrivine <balagrivine@gmail.com>
2023-06-21 18:16:56 +0300

    correction to comply to betty coding style

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -21,13 +21,13 @@ void print_to_98(int n)
 			else
 				printf("%d, ", n);
 		}
-	
+
 	}
 	else
 	{
 		for (; n >= 98; n--)
 		{
-			if (n ==98)
+			if (n == 98)
 			{
 				printf("%d", n);
 				printf("\n");
