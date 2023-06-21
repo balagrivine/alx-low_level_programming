407e4e80e6ef6838235b92676d29aa1be22c9f60
balagrivine <balagrivine@gmail.com>
2023-06-21 22:20:11 +0300

    my first commit -

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * positivee_or_negative - determines whether a number is positive or negative
+ *
+ * @i: enter number as integer
+ *
+ * Return: always 0 (success)
+ */
+
+int positive_or_negative(int i)
+{
+	if (i < 0)
+		printf("%d is negative", i);
+
+	if (i == 0)
+		printf("%d is zero", i);
+	else
+		printf("%d is positive", i);
+	return (i);
+
+}
