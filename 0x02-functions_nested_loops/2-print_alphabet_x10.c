0c71203145ebdefe8416a1acceb9cba6526f851a
balagrivine <balagrivine@gmail.com>
2023-06-21 11:45:44 +0300

    followed betty coding style

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,14 +8,16 @@
 void print_alphabet_x10(void)
 {
 	char letter;
+
 	int line_count;
+
 	line_count = 0;
 
 	while (line_count <= 9)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 		{
-		       _putchar(letter);
+			_putchar(letter);
 
 		}
 		_putchar('\n');
