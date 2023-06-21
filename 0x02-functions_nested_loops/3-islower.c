9ef307e552884711eb4aac8b551869551d5bc7bc
balagrivine <balagrivine@gmail.com>
2023-06-21 12:55:53 +0300

    check if a letter is lower case

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 /**
  * _islower - checks if a character is lower case
  * _putchar - prints th arguments passed to it
- * int c - takes argument and tells whether it is lowercase or not
+ * int - takes argument and tells whether it is lowercase or not
  *
  * Return: returns 1 if character is lower case
  * returns 0 if character is not lower case
@@ -11,17 +11,13 @@
 int _islower(int c)
 {
 
-	while (c >= 'a')
+	if (c >= 97 && c <= 122)
 	{
-		if (c <= 'z')
-		{
-			_putchar(c);
-			return (1);
-		}
-		if (c >= 'z')
-			return (0);
+		return (1);
 	}
+	else
+		return (0);
+
 	_putchar('\n');
-	return (0);
 
 }
