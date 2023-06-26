2373cabf5e8e9a25170bf112550cbce51e7dbb47
balagrivine <balagrivine@gmail.com>
2023-06-26 02:31:56 -0700

    Betty documentation style

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * swap_int : swaps values of two integers
+ * swap_int - swaps values of two integers
  * @a: first integer to be swapped
  * @b: second int to be swapped
  * Return: nothing
  */
 
 void swap_int(int *a, int *b)
-/*The function that swaps values of two integers*/
+/*The function that swaps values of two integers.*/
 {
 	int temp = *a;
 	*a = *b;
