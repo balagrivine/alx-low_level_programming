fc6e5d24612e594d08d06e57702b6c8b2c9b3f12
balagrivine <balagrivine@gmail.com>
2023-06-26 02:20:06 -0700

    reset value of avariable using pointers

---
+++ 0x05-pointers_arrays_strings/0-putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to the standard output
+ *
+ * Return: return 1 (success)
+ * return -1 on error
+ */
+
+int _putchar(char c)
+{
+        return (write(1, &c, 1));
+
+}
---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * reset_to_98 - resets the value of a variable to 98
+ * @n:enter value as integer
+ * Return: returns no value
+ */
+
+void reset_to_98(int *n)
+
+{
+
+	*n = 98;
+
+}
