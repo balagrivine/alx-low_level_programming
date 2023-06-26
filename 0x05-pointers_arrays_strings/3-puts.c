8c13e2b14ef3bae360257e22b5088c41fba422f1
balagrivine <balagrivine@gmail.com>
2023-06-26 03:11:57 -0700

    print string followed by a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _Puts - prints a string followed by qa new line t the standard output
+ * @str: string t be printed
+ *Return: nothing
+ */
+
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
