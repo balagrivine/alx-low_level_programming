a7632d33bf787dbea1dc10d1ef85630046465584
balagrivine <balagrivine@gmail.com>
2023-06-29 01:41:15 -0700

    appent n number of characters from one string to annother

---
+++ 0x06-more_pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _strncat - appends at most n bytes from string src to string dest
+ * @src: copy from
+ *@dest: copy to
+ *@n: first n characters to be copied from src
+ *Return: pointer to dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+
+{
+	int i;
+	int j;
+
+	i = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	j = 0;
+
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
