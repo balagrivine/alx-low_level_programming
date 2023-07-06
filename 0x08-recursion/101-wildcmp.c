bd60f2a0d0644d1702adfbda53c31a3a354e5cc0
balagrivine <balagrivine@gmail.com>
2023-07-06 11:37:57 -0700

    write a function that compares two strings

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -10,7 +10,7 @@
 int wildcmp(char *s1, char *s2)
 
 {
-	if (*s2 == '*' && *(s2 + 1) == '\0' && *s1 == '\0')
+	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
 	{
 		return (0);
 	}
@@ -27,7 +27,7 @@ int wildcmp(char *s1, char *s2)
 
 	if (*s2 == '*')
 	{
-		return (wildcmp(s1, (s2 + 1))) || wildcmp(s1 + 1, s2);
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 	}
 	return (0);
 }
