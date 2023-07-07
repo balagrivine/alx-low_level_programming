76eaa9c0300924b076564f399f7eafb06c430d57
balagrivine <balagrivine@gmail.com>
2023-07-07 09:56:49 -0700

    make my code comply to betty coding style

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,14 +7,14 @@
  * Return: always (0) success
  */
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 
 {
 	int i;
 
 	if (argc == 1)
 	{
-	
+
 		for (i = 0; i < argc; i++)
 		{
 			printf("%s\n", argv[i]);
