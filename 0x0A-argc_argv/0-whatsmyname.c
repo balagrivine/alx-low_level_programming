92f2a22ad686e8376c296d0213ab4401853f61f8
balagrivine <balagrivine@gmail.com>
2023-07-07 09:53:55 -0700

    write a program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name followed by a new line
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: always (0) success
+ */
+
+int main(int argc, char** argv)
+
+{
+	int i;
+
+	if (argc == 1)
+	{
+	
+		for (i = 0; i < argc; i++)
+		{
+			printf("%s\n", argv[i]);
+		}
+	}
+
+	return (0);
+
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This directory contains all related to to arguments passed to the main function such as the argumeent count and the argumen vector
