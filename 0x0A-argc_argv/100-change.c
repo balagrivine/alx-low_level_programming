748f53e52abfceb87c6cde71f8c48d597f581832
balagrivine <balagrivine@gmail.com>
2023-07-07 14:20:39 -0700

    write a program that find the change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - dd
+ * @argc: hj
+ * @argv: kk
+ * Return: mm
+ */
+
+int main(int argc, char **argv)
+{
+	int cents, coins;
+
+	coins = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	cents = atoi(argv[1]);
+	while (cents > 0)
+	{
+		coins++;
+		if ((cents - 25) >= 0)
+		{
+			cents -= 25;
+			continue;
+		}
+		if ((cents - 10) >= 0)
+		{
+			cents -= 10;
+			continue;
+		}
+		if ((cents - 5) >= 0)
+		{
+			cents -= 5;
+			continue;
+		}
+		if ((cents - 2) >= 0)
+		{
+			cents -= 2;
+			continue;
+		}
+		if ((cents - 1) >= 0)
+		{
+			cents -= 1;
+			continue;
+		}
+		cents--;
+	}
+	printf("%d\n", coins);
+	return (0);
+}
