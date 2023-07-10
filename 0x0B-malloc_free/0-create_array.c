57fcb7d1e812e9b66b90badb290a4f0ea15abe1f
balagrivine <balagrivine@gmail.com>
2023-07-10 05:46:41 -0700

    write a function tht creates an array of characters and initializes it with a specific character

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of
+ * characters and initializes with a special char
+ * @size: size of the array
+ * @c: specific char that initializes the array
+ * Return: pointer to the array or null if it fails
+ */
+
+char *create_array(unsigned int size, char c)
+
+{
+	(void)c;
+	char *arr;
+
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	else
+	{
+	arr = malloc(sizeof(char) * size);
+
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = c;
+	}
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+
+	}
+	return (arr);
+
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+This directory contains files with tasks on dynamic memory allocation as well as freeing the allocated memory
