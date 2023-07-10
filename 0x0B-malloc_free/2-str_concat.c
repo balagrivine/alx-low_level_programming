98b689a4d5d40ad108a2f4ecea0efa52f9671e29
balagrivine <balagrivine@gmail.com>
2023-07-10 08:57:52 -0700

    comply to betty coding style

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,53 +1,35 @@
 #include "main.h"
-
 #include <stdlib.h>
-
 /**
  * str_concat - concatenates two strings
  * @s1: string 1
  * @s2: string 2
  * Return: pointer to a string which contains s1 and s2
  */
-
 char *str_concat(char *s1, char *s2)
 {
 	char *arr;
-	int i, a, b, c;
-
-	a = 0;
-
-	b = 0;
-
-	c = 0;
-
-	i = 0;
+	int i = 0, a = 0, b = 0, c = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
 	while (s1[i])
 	{
 		i++;
 	}
-
 	while (s2[a])
 	{
 		a++;
 	}
-
 	c = i + a;
-
 	arr = malloc((sizeof(char) * c) + 1);
-
 	if (arr == NULL)
 	{
 		return (NULL);
 	}
-
 	a = 0;
-
 	while (b < c)
 	{
 		if (b <= i)
@@ -61,7 +43,6 @@ char *str_concat(char *s1, char *s2)
 		}
 		b++;
 	}
-
 	arr[b] = '\0';
 	return (arr);
 }
