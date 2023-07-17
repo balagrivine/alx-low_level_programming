cf0f190c6e9676c5f5f069fc81a5eb088d103bc9
balagrivine <balagrivine@gmail.com>
2023-07-17 05:11:30 -0700

    comply to betty documentation style of structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
+/*struct dog - new data type
+* @name: name of the dog
+* @age: age of the dog
+* @owner: owner of the dog
+*
+* Description: This structure contains details about the dog
+*/
 struct dog
 {
 	char *name;
