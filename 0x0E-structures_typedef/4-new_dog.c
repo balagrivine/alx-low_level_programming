f193cee28f89d3d86ae7cee0a36f2ba1ba430735
balagrivine <balagrivine@gmail.com>
2023-07-18 01:55:32 -0700

    comply to betty coding style

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,11 +13,12 @@ int _strlen(char *s)
 	int i;
 
 	i = 0;
+
 	while (s[i] != '\0')
 	{
 		i++;
 	}
-	return(i);
+	return (i);
 }
 
 /**
@@ -31,6 +32,7 @@ char *_strcpy(char *dest, char *src)
 	int len, i;
 
 	len = 0;
+
 	while (src[len] != '\0')
 	{
 		len++;
@@ -52,6 +54,7 @@ char *_strcpy(char *dest, char *src)
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *doggy;
+
 	if (name == NULL || age < 0 || owner == NULL)
 		return (NULL);
 	doggy = malloc(sizeof(dog_t));
