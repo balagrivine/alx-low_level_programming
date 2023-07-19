4dfafb31e36c783e05a8f673484363c99e545d9b
balagrivine <balagrivine@gmail.com>
2023-07-19 03:46:06 -0700

    write aa fnction that searches an integer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ * @array: array ofmintegers to be looped over
+ * @size: sizeof the array
+ * @cmp: pointer to function to be used to compare values
+ * Return: index of the first element for which the cmp function does not rturn 0
+ * return -1 if functions fail and if size<=0
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array != NULL && cmp != NULL && size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+			return (i);
+		}
+	}
+	return (-1);
+
+}
