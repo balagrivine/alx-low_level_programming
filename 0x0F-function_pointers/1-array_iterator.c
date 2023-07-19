6339ecb55192faf4775b9e2aa9404539a96611ee
balagrivine <balagrivine@gmail.com>
2023-07-19 14:18:59 -0700

    comply to bettty coding and documentation style

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 
 /**
- * array_iterator -  executes a function given as a parameter on each element of an array.
+ * array_iterator -  executes a function
+ *given as a parameter on each element of an array.
  * @size: size of the array
  * @action:  pointer to function
  */
