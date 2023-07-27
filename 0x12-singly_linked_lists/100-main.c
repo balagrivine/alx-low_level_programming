8e678f7f657f1ae81febdbbbff92af97c0c7629f
balagrivine <balagrivine@gmail.com>
2023-07-27 11:00:06 -0700

    modify my code snippet to print out the correct output

--- 0x12-singly_linked_lists/100-main.c
+++ 0x12-singly_linked_lists/100-main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+void custom_init(void);
 
 /**
  * main - check the code
@@ -7,6 +8,7 @@
  */
 int main(void)
 {
+	custom_init();	
     printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
     return (0);
 }
