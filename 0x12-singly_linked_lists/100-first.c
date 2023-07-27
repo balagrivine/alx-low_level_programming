f54cc143328a431aba022b1eddb6ee09140627c1
balagrivine <balagrivine@gmail.com>
2023-07-27 10:50:37 -0700

    write a custom initialization functon

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+
+/**
+ * first - writes first to the console before the main function runs
+ *
+ * Return: void
+ */
+
+void custom_init(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
