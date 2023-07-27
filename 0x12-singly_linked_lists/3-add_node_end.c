0b4cd35d725ead850de8c76c313b2f1e1f21ee11
balagrivine <balagrivine@gmail.com>
2023-07-27 10:05:06 -0700

    write a function to append a new node at the end of a linked list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,27 +11,39 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	/*list_t *temp;*/
-	list_t *new_node;
-	unsigned int i = 0;
+	list_t *temp, *new_node;
 
-	while (str[i])
+	new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
 	{
-		i++;
+		return (NULL);
 	}
-		new_node = malloc(sizeof(list_t));
-		if (new_node == NULL)
-		{
-			return (NULL);
-		}
-		new_node->str = strdup(str);
-		new_node->str = NULL;
 
-		if (*head == NULL)
+	new_node->str = strdup(str);
+
+	if (new_node->str == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node->len = strlen(str);
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		
+		*head = new_node;
+	}
+	else
+	{
+		temp = *head;
+		while (temp->next != NULL)
 		{
-			*head = new_node;
-			return (new_node);
+			temp = temp->next;
 		}
+		temp->next = new_node;
+	}
 
 	return (new_node);
 }
