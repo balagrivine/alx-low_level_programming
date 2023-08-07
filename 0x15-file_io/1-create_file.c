42480c7e5eb99dfc18a39b231fc23f6beb68a9dd
balagrivine <balagrivine@gmail.com>
2023-08-07 05:52:54 -0700

    write a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,6 +28,10 @@ int create_file(const char *filename, char *text_content)
 			count++;
 		}
 	}
+	else
+	{
+		file = open(filename, O_CREAT || O_RDWR);
+	}
 	
 	file = open(filename, O_CREAT | O_RDWR | O_TRUNC);
 
