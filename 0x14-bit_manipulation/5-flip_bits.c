08de038dcf710cf59c372a1396172785a760f6fb
balagrivine <balagrivine@gmail.com>
2023-08-31 05:19:27 -0700

    make my code betty compliant

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,7 +10,7 @@
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	int num_bits = 0;
-	unsigned long int temp = ( n ^ m);
+	unsigned long int temp = (n ^ m);
 
 	while (temp > 0)
 	{
