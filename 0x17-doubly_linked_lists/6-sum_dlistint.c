0e9792ac42a95f53080adf4c527ba25ebb2df454
balagrivine <balagrivine@gmail.com>
2023-10-12 14:09:15 -0700

    wrte a function that returns the sum of all data of a DLL

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * sum_dlistint - returns sum of all the data of a DLL
+ * @head: pointer to the head of the list
+ * Return: sum
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+	int sum;
+
+	sum = 0;
+	temp = head;
+	while (temp)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+	return sum;
+}
