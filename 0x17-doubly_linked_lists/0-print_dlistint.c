3860cf27ff702c7e801d9b2ed43f1d08f73a8621
balagrivine <balagrivine@gmail.com>
2023-10-12 13:04:29 -0700

    write a function that retutns the no. of elements in  a DLL

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/**
+ * print_dlistint - prints eements of a linked list
+ * @h: pointer to the head of the linked list
+ * Return: number of elements
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count;
---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * dlistint_len - returns no. of elements
+ * @h: pointer to the head of the DLL
+ * Return: no. of elements
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t len;
+	const dlistint_t *temp;
+
+	temp = h;
+	while (temp)
+	{
+		len++;
+		temp = temp->next;
+	}
+	return len;
+}
