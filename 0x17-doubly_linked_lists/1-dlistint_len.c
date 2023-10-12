b2fc949c80ee2066e38cf4b02add996764251671
balagrivine <balagrivine@gmail.com>
2023-10-12 12:45:22 -0700

    write a function that returns elements of a doubly linked list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *  * dlistint_len - returns the number of elements in a linked list
+ *   * @h: pointer to the head of the list
+ *    *
+ *     * Return: number of elements in the list
+ *      */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t len;
+	const dlistintT *temp;
+
+	temp = h;
+	len = 0;
+	while (temp)
+	{
+		len++;
+		temp = temp->next;
+	}
+	return len;
+}
