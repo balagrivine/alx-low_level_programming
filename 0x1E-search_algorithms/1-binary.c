7cafdee91a38ac6ab98b39928662581c76b6c390
balagrivine <balagrivine@gmail.com>
2024-02-05 08:38:53 -0800

    write a function that searches for a value in a sorted array of integers using the Binary search algorithm

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -20,11 +20,12 @@ int binary_search(int *array, size_t size, int value)
 	{
 		if (array == NULL)
 			return -1;
-		mid = size / 2;
+		mid = low + (high - low)/2
 		if(array[mid] == value)
 			return mid;
 		if(array[mid] > value)
 			return binary_search(array, mid - 1, value);
+		else
 		return binary_search(array, mid + 1, value);
 	}
 	return -1;
