ce1add74a61062d93d928fd3092707787a565419
balagrivine <balagrivine@gmail.com>
2024-02-05 07:39:55 -0800

    document my linear search file

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,12 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - function that searches for 
+ * a value in an array of integers using linear search
+ * @array: pointer to an arry of integers
+ * @size: size of the array
+ * @value: value to be searched for
+ * Return: value if present, -1 if array is NULL or value absent
+ */
+
+int linear_search(int *array, size_t size, int value)
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,4 +4,6 @@
 #include <stdio.h>
 #include <stddef.h>
 
+int linear_search(int *array, size_t size, int value);
+
 #endif
