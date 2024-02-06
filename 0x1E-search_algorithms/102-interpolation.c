669dd0f1470f3982a4dd3545ca9a8382e6510f46
balagrivine <balagrivine@gmail.com>
2024-02-06 05:33:20 -0800

    Write a function that searches for a value in a sorted array of integers using the Exponential search algorithm

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,38 @@
+#include "search_algos.h"
+#include <math.h>
+
+/**
+ * exponential_search - a function that searches for 
+ * a value in a sorted array
+ * @array: pointer to the first elemet of the array
+ * @size: number of elements of the array
+ * @value: value to search for
+ * Return: -1 if value is absent else return value
+ */
+
+int exponential_search(int *array, size_t size, int value)
+{
+	int low, high, pos;
+
+	low = 0;
+	high = size - 1;
+	pos = low + (value - array[low])*(high - low)/(array[high] - array[low]);
+
+	if (array == NULL)
+		return -1;
+
+	while (low <= high && value >= array[low] && value <= array[high])
+	{
+		if (array[pos] == value)
+		{
+			return value;
+		}
+
+		if (array[pos] < value)
+			low = pos + 1;
+		else
+			high = pos - 1;
+	}
+	return -1;
+
+}
