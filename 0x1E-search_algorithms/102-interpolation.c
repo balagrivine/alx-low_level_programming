52be46774a89c5db69e1ef1ec9f92b7fdf2153b3
balagrivine <balagrivine@gmail.com>
2024-02-06 06:05:40 -0800

    correct

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -16,13 +16,13 @@ int interpolation_search(int *array, size_t size, int value)
 
 	low = 0;
 	high = size - 1;
-	pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
 
 	if (array == NULL)
 		return -1;
 
 	while (low <= high && value >= array[low] && value <= array[high])
 	{
+		pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
 		if (array[pos] == value)
 		{
 			return pos;
