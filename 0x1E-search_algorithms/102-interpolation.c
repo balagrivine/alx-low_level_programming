6094820ee65fcbbef3cd6c3ff741aad98678b48e
balagrivine <balagrivine@gmail.com>
2024-02-06 06:35:26 -0800

    make

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -30,18 +30,19 @@ int interpolation_search(int *array, size_t size, int value)
 
 		if (array[pos] < value)
 		{
-			printf("Value checked array[%lu] = [%d]", pos, array[pos]);
+			printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
 			low = pos + 1;
-			printf("Value checked array[%lu] = [%d]", low, array[low]);
+			printf("Value checked array[%lu] = [%d]\n", low, array[low]);
 		}
 		else
 		{
-			printf("Value checked array[%lu] = [%d]", pos, array[pos]);
+			printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
 			high = pos - 1;
-			printf("Value checked array[%lu] = [%d]", high, array[high]);
+			printf("Value checked array[%lu] = [%d]\n", high, array[high]);
 		}
 		i++;
 	}
+	printf("Value checked array[%lu] is out of range", pos)'
 	return -1;
 
 }
