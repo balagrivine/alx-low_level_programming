dc3a70652e6d72bde159802d836dba297acd0daa
balagrivine <balagrivine@gmail.com>
2024-02-06 09:41:41 -0800

    write

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,19 @@
+#include "search_algos.h"
+
+/**
+ * advanced_binary - searches for an element in an ordered
+ * list using binary search
+ * @array: pointer to the first element
+ * @size: number of elemnts in the array
+ * @value: value to be searched for
+ * Return: -1 if value absent or array NULL else index where value
+ * is located
+ */
+
+int advanced_binary(int *array, size_t size, int value)
+{
+	size_t low, high;
+
+	if (array == NULL)
+		return -1;
+}
