d4d97ced170da70d5ed8d928bac71c226a4fbf81
balagrivine <balagrivine@gmail.com>
2024-02-07 06:55:33 -0800

    binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -16,7 +16,7 @@ int binary_search(int *array, size_t size, int value)
 	if(array == NULL)
 		return -1;
 
-	for (low = 0, high = size - 1;high >= low)
+	for (low = 0, high = size - 1;high >= low;)
 	{
 		mid = low + (high - low)/2;
 		printf("Searching in array :");
